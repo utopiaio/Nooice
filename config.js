27028de601519c4d854c489f948458970400b936
Moe Szyslak <moe.duffdude@gmail.com>
2017-03-04 06:38:17 +0300

    Nooice!

---
+++ config.js
@@ -0,0 +1,21 @@
+// database
+const DB_HOST = '127.0.0.1';
+const DB_USER = 'moe';
+const DB_PASSWORD = '';
+const DB_PORT = 5432;
+const DB_NAME = 'showtime';
+
+// app
+const APP_HOST = '0.0.0.0';
+const APP_PORT = '8000';
+
+module.exports = {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_NAME,
+
+  APP_HOST,
+  APP_PORT,
+};
