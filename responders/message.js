b1bdfae772d48a777cf9d4c9dc543a3299f530ba
Moe Szyslak <moe.duffdude@gmail.com>
2017-03-05 10:13:10 +0300

    nooice

--- responders/message.js
+++ responders/message.js
@@ -78,11 +78,8 @@ ${moment(atm.atm_timestamp).format('MMMM DD, YYYY')}
 ${atm.atm_approved ? '✅' : '⏳'}
 
 /location_${atm.atm_id}
-
 /approve_${atm.atm_id}
-
 /disapprove_${atm.atm_id}
-
 /delete_${atm.atm_id}`).join(`
 
 
@@ -97,6 +94,23 @@ ${atm.atm_approved ? '✅' : '⏳'}
       });
     });
 
+    return;
+  }
+
+  if (msg.text.search(/^\/location_\d+$/) === 0) {
+    const atmId = Number.parseInt(msg.text.match(/^\/location_(\d+)$/)[1], 10);
+
+    moedoo.query('SELECT ST_AsGeoJSON(atm_location) as atm_location FROM atm WHERE atm_id = $1', [atmId]).then((rows) => {
+      if (rows.length === 1) {
+        const atm = rows[0];
+        bot.sendLocation(msg.chat.id, JSON.parse(atm.atm_location).coordinates[0], JSON.parse(atm.atm_location).coordinates[1]);
+        return;
+      }
+
+      bot.sendMessage(msg.chat.id, 'NOOICE?');
+    }, (err) => {
+      console.log(err);
+    });
     return;
   }
 
