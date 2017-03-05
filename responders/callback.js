b4587bbfbc72b6d57dfeeb352201989e3a5ed923
Utopia፲ <utopiaio@yahoo.com>
2017-03-05 07:38:24 +0300

    nooice

--- responders/callback.js
+++ responders/callback.js
@@ -11,14 +11,15 @@ module.exports = (bot, config, moedoo) => (callbackQuery) => {
       return;
 
     case 'S':
-      // preparing query is a bit _tricky_ (with the conversion n' all)
-      // so I'm going old school
+      // preparing query is a bit _tricky_ (with the ST conversion n' all), so I'm going old school
       if (`${data.l.latitude} ${data.l.longitude}`.search(/^\d+\.\d+ \d+\.\d+$/) === 0) {
         moedoo.query(`
-          SELECT atm_bank_name,
-                ST_AsGeoJSON(atm_location) as atm_location,
-                round(CAST(ST_Distance_Spheroid(atm_location, ST_GeomFromGeoJSON('{"type": "point", "coordinates": [${data.l.latitude}, ${data.l.longitude}]}'), 'SPHEROID["WGS 84",6378137,298.257223563]') as numeric), 0) as atm_distance
+          SELECT atm_id,
+                 atm_bank_name,
+                 ST_AsGeoJSON(atm_location) as atm_location,
+                 round(CAST(ST_Distance_Spheroid(atm_location, ST_GeomFromGeoJSON('{"type": "point", "coordinates": [${data.l.latitude}, ${data.l.longitude}]}'), 'SPHEROID["WGS 84",6378137,298.257223563]') as numeric), 0) as atm_distance
           FROM atm
+          WHERE atm_approved = true
           ORDER BY atm_location <-> ST_GeomFromGeoJSON('{"type": "point", "coordinates": [${data.l.latitude}, ${data.l.longitude}]}')
           LIMIT 3;
         `).then((rows) => {
