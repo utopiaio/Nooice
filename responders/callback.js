450f060554c02e314b5f38e8eeef0c7f7f2f6943
Moe Szyslak <moe.duffdude@gmail.com>
2017-03-05 07:16:01 +0300

    nooice

--- responders/callback.js
+++ responders/callback.js
@@ -1,7 +1,7 @@
 /* eslint no-console: 0 */
 
 module.exports = (bot, config, moedoo) => (callbackQuery) => {
-  console.log(callbackQuery, moedoo);
+  console.log(callbackQuery);
 
   const data = JSON.parse(callbackQuery.data);
 
@@ -12,6 +12,18 @@ module.exports = (bot, config, moedoo) => (callbackQuery) => {
 
     case 'S':
       bot.answerCallbackQuery(callbackQuery.id, 'NOOICE!', false);
+      moedoo.query(`
+        SELECT atm_bank_name,
+              ST_AsGeoJSON(atm_location) as atm_location,
+              round(CAST(ST_Distance_Spheroid(atm_location, ST_GeomFromGeoJSON('{"type": "point", "coordinates": [$1, $2]}'), 'SPHEROID["WGS 84",6378137,298.257223563]') as numeric), 0) as atm_distance
+        FROM atm
+        ORDER BY atm_location <-> ST_GeomFromGeoJSON('{"type": "point", "coordinates": [$1, $2]}')
+        LIMIT 3;
+      `, [data.l.latitude, data.l.longitude]).then((rows) => {
+        console.log(rows);
+      }, (err) => {
+        console.log(err);
+      });
       return;
 
     case 'A':
