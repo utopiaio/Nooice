2a1e22000a53ea1eb8e65fc7b91e0072ffb55863
Moe Szyslak <moe.duffdude@gmail.com>
2017-03-07 11:50:29 +0300

    nooice

--- commands/location.js
+++ commands/location.js
@@ -4,13 +4,18 @@ module.exports = (bot, msg, moedoo) => {
   const atmId = Number.parseInt(msg.text.match(/^\/location_(\d+)$/)[1], 10);
 
   moedoo
-    .query('SELECT ST_AsGeoJSON(atm_location) as atm_location FROM atm WHERE atm_id = $1', [atmId])
+    .query('SELECT atm_bank_name, ST_AsGeoJSON(atm_location) as atm_location FROM atm WHERE atm_id = $1', [atmId])
     .then((rows) => {
       if (rows.length === 1) {
         const atm = rows[0];
 
-        // eslint-disable-next-line
-        bot.sendLocation(msg.chat.id, JSON.parse(atm.atm_location).coordinates[0], JSON.parse(atm.atm_location).coordinates[1]);
+        bot.sendMessage(msg.chat.id, `🏦 ${atm.atm_bank_name}`, {
+          disable_notification: true,
+        }).then(() => {
+          // eslint-disable-next-line
+          bot.sendLocation(msg.chat.id, JSON.parse(atm.atm_location).coordinates[0], JSON.parse(atm.atm_location).coordinates[1]);
+        });
+
         return;
       }
 
