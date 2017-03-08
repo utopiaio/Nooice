c9803153f309fdca480f9e98ba825cf50b04e618
Moe Szyslak <moe.duffdude@gmail.com>
2017-03-08 03:09:32 +0300

    nooice

--- commands/unregister.js
+++ commands/unregister.js
@@ -2,10 +2,20 @@ module.exports = (bot, msg, moedoo) => {
   bot.sendChatAction(msg.chat.id, 'typing');
 
   moedoo
-    .query('DELETE FROM nooice WHERE nooice_id=$1', [msg.from.id])
+    .query('DELETE FROM nooice WHERE nooice_id=$1 returning nooice_id;', [msg.from.id])
     .then((rows) => {
       console.log(rows);
-      bot.sendMessage(msg.chat.id, 'NOOICE 👍🏿');
+
+      bot.sendMessage(msg.chat.id, 'NOOICE 👍🏿', {
+        reply_markup: JSON.stringify({
+          keyboard: [
+            [{ text: 'Send 📍', request_location: true }],
+            [{ text: 'ቀን / Date' }, { text: 'NOOICE!' }],
+          ],
+          resize_keyboard: true,
+          one_time_keyboard: false,
+        }),
+      });
     }, () => {
       bot.sendMessage(msg.chat.id, 'NOOICE? Unable to unregister 😔');
     });
