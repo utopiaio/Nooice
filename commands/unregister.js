bf5c0a8e1ff30a7199a1b26bccfbb72bd4b67c44
Moe Szyslak <moe.duffdude@gmail.com>
2017-03-08 06:26:18 +0300

    nooice

--- commands/unregister.js
+++ commands/unregister.js
@@ -4,22 +4,16 @@ module.exports = (bot, msg, moedoo) => {
   moedoo
     .query('DELETE FROM nooice WHERE nooice_id=$1 returning nooice_id;', [msg.from.id])
     .then((rows) => {
-      bot.sendMessage(msg.chat.id, rows.length === 1 ? `NOOICE 😔
-
-You've successfully *unregistered*
-
-*Thank you* for all your contributions. From now on whenever you send me your location I'll reply with nearby 🏧s` : `NOOICE?
-You have to be a register to *unregister*`, {
-  parse_mode: 'Markdown',
-  reply_markup: JSON.stringify({
-    keyboard: [
-      [{ text: 'Send 📍', request_location: true }],
-      [{ text: 'ቀን / Date' }, { text: 'NOOICE!' }],
-    ],
-    resize_keyboard: true,
-    one_time_keyboard: false,
-  }),
-});
+      bot.sendMessage(msg.chat.id, rows.length === 1 ? 'NOOICE 😔You\'ve successfully unregistered\n\nThank you for all your contributions. From now on whenever you send me your location I\'ll reply with nearby 🏧s' : 'NOOICE?\nYou have to register to unregister', {
+        reply_markup: JSON.stringify({
+          keyboard: [
+            [{ text: 'Send 📍', request_location: true }],
+            [{ text: 'ቀን / Date' }, { text: 'NOOICE!' }],
+          ],
+          resize_keyboard: true,
+          one_time_keyboard: false,
+        }),
+      });
     }, () => {
       bot.sendMessage(msg.chat.id, 'NOOICE? Unable to unregister 😔');
     });
