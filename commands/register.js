271b23988f530ce68b7e0cf263762cc8b663aae2
Moe Szyslak <moe.duffdude@gmail.com>
2017-03-08 06:36:36 +0300

    nooice

--- commands/register.js
+++ commands/register.js
@@ -4,8 +4,26 @@ module.exports = (bot, msg, moedoo) => {
   moedoo
     .query('INSERT INTO nooice (nooice_id) VALUES ($1)', [msg.from.id])
     .then(() => {
-      bot.sendMessage(msg.chat.id, 'NOOICE!\nYou are now a contributor 🙌🏿\n\nWhenever you send me your location I\'ll ask you if you want to register an 🏧');
+      bot.sendMessage(msg.chat.id, 'NOOICE!\nYou are now a contributor 🙌🏿\n\nWhenever you send me your location I\'ll ask you if you want to register an 🏧', {
+        reply_markup: JSON.stringify({
+          keyboard: [
+            [{ text: 'Send 📍', request_location: true }],
+            [{ text: 'ቀን / Date' }, { text: 'NOOICE!' }],
+          ],
+          resize_keyboard: true,
+          one_time_keyboard: false,
+        }),
+      });
     }, () => {
-      bot.sendMessage(msg.chat.id, 'DOUBLE NOOICE!\nTho you\'re already a contributor 🙌🏿\n\nPS\nTo unregister send /unregister command');
+      bot.sendMessage(msg.chat.id, 'DOUBLE NOOICE!\nTho you\'re already a contributor 🙌🏿\n\nPS\nTo unregister send /unregister command', {
+        reply_markup: JSON.stringify({
+          keyboard: [
+            [{ text: 'Send 📍', request_location: true }],
+            [{ text: 'ቀን / Date' }, { text: 'NOOICE!' }],
+          ],
+          resize_keyboard: true,
+          one_time_keyboard: false,
+        }),
+      });
     });
 };
