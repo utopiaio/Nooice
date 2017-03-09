cf132e5b297c62ec23fcfd392f63d2622c63beb9
Moe Szyslak <moe.duffdude@gmail.com>
2017-03-09 21:29:00 +0300

    nooice

--- commands/browse.js
+++ commands/browse.js
@@ -5,7 +5,7 @@ module.exports = (config, bot, chatId, location, moedoo) => {
       const total = Math.ceil(Number.parseInt(atmsCount[0].atm_count, 10) / config.PER_PAGE);
 
       moedoo
-        .query(`SELECT atm_id, atm_bank_name, ST_AsGeoJSON(atm_location) as atm_location, round(CAST(ST_Distance_Spheroid(atm_location, ST_GeomFromGeoJSON('{"type": "point", "coordinates": [${location.latitude}, ${location.longitude}]}'), 'SPHEROID["WGS 84",6378137,298.257223563]') as numeric), 0) as atm_distance FROM atm WHERE atm_approved = true ORDER BY atm_location <-> ST_GeomFromGeoJSON('{"type": "point", "coordinates": [${location.latitude}, ${location.longitude}]}') LIMIT ${config.PER_PAGE};`)
+        .query(`SELECT atm_id, atm_bank_name, ST_AsGeoJSON(atm_location) as atm_location, round(CAST(ST_Distance_Spheroid(atm_location, ST_GeomFromGeoJSON('{"type": "point", "coordinates": [${location[0]}, ${location[1]}]}'), 'SPHEROID["WGS 84",6378137,298.257223563]') as numeric), 0) as atm_distance FROM atm WHERE atm_approved = true ORDER BY atm_location <-> ST_GeomFromGeoJSON('{"type": "point", "coordinates": [${location[0]}, ${location[1]}]}') LIMIT ${config.PER_PAGE};`)
         .then((atms) => {
           const msg = atms.map(atm => `🏦 ${atm.atm_bank_name} 🏧\n📍 ${Number.parseInt(atm.atm_distance, 10).toLocaleString('us')} meter${Number.parseInt(atm.atm_distance, 10) > 1 ? 's' : ''}\n/location_${atm.atm_id}`).join('\n\n');
 
@@ -16,9 +16,9 @@ module.exports = (config, bot, chatId, location, moedoo) => {
           bot.sendMessage(chatId, msg, {
             reply_markup: JSON.stringify({
               inline_keyboard: [[
-                { text: '←', callback_data: JSON.stringify({ type: 'NAV', t: total, d: -1, c: 0, l: [location.latitude, location.longitude] }) },
+                { text: '←', callback_data: JSON.stringify({ type: 'NAV', t: total, d: -1, c: 0, l: [location[0], location[1]] }) },
                 { text: `1 of ${total}`, callback_data: JSON.stringify({ type: 'STY' }) },
-                { text: '→', callback_data: JSON.stringify({ type: 'NAV', t: total, d: 1, c: 0, l: [location.latitude, location.longitude] }) },
+                { text: '→', callback_data: JSON.stringify({ type: 'NAV', t: total, d: 1, c: 0, l: [location[0], location[1]] }) },
               ]],
             }),
           });
