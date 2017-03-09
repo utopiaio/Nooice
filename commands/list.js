588b66b2ba983737853811bc14001d999210ae7b
Utopia፲ <utopiaio@yahoo.com>
2017-03-09 21:51:47 +0300

    nooice?

--- commands/list.js
+++ commands/list.js
@@ -4,7 +4,7 @@ module.exports = (bot, msg, moedoo) => {
   bot.sendChatAction(msg.chat.id, 'typing');
 
   moedoo
-    .query('SELECT atm_id, atm_bank_name, ST_AsGeoJSON(atm_location) as atm_location, atm_timestamp, atm_approved FROM atm ORDER BY atm_id DESC')
+    .query('SELECT atm_id, atm_bank_name, ST_AsGeoJSON(atm_location) as atm_location, atm_timestamp, atm_approved FROM atm ORDER BY atm_id DESC;')
     .then((rows) => {
       const approvedATMs = rows.filter(atm => atm.atm_approved);
       const message = rows.slice(0, 10).map(atm => `${atm.atm_bank_name}\n${moment(atm.atm_timestamp).utc().add(3, 'hours').format('MMMM DD, YYYY')}\n${moment(atm.atm_timestamp).utc().add(3, 'hours').format('HH:mm:ss')}\n${atm.atm_approved ? '✅' : '⏳'}\n\n/location_${atm.atm_id}\n/approve_${atm.atm_id}\n/disapprove_${atm.atm_id}\n/delete_${atm.atm_id}`).join('\n\n\n');
