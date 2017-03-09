afc29880d89ddc7d0f33b23da4eb58b414412983
Moe Szyslak <moe.duffdude@gmail.com>
2017-03-09 20:28:34 +0300

    nooice?

--- commands/approve.js
+++ commands/approve.js
@@ -4,7 +4,7 @@ module.exports = (bot, msg, moedoo) => {
   const atmId = Number.parseInt(msg.text.match(/^\/approve_(\d+)$/)[1], 10);
 
   moedoo
-    .query('UPDATE atm SET atm_approved=$1 WHERE atm_id=$2', [true, atmId])
+    .query('UPDATE atm SET atm_approved=$1 WHERE atm_id=$2;', [true, atmId])
     .then(() => {
       bot.sendMessage(msg.chat.id, 'NOOICE 👍🏿');
     }, () => {
